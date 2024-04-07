2efa9f81ba2a6e66983fcbb9bd0ea0f9873b3849
Miguel Carrasco <quintes32-miguel@hotmail.es>
2024-04-07 16:09:48 +0200

    logic fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Operacion from './components/Operacion';
+import imageCalc from './assets/smiley-calculator.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title'>¡Hoy la calculadora vas a ser tú!</h1>
+        <img src={imageCalc} className='img-home' alt='calculator img'></img>
+        <Router>
+          <div>
+            <ul className='nav-links'>
+            <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/sumas">Practica sumas</Link>
+              </li>
+              <li>
+                <Link to="/restas">Practica restas</Link>
+              </li>
+              <li>
+                <Link to="/multiplicaciones">Practica multiplicaciones</Link>
+              </li>
+              <li>
+                <Link to="/divisiones">Practica divisiones</Link>
+              </li>
+              
+            </ul>
+          </div>
+          <Routes>
+          <Route path="/sumas" element={<  Operacion op="+"/>}   />
+          <Route path="/restas" element={<Operacion op="-"/>}   />
+          <Route path="/multiplicaciones" element={<Operacion op="*"/>}   />
+          <Route path="/divisiones" element={<Operacion op="/"/>}/>
+          
+          
+          </Routes>
+        </Router>
       </header>
+      
     </div>
   );
 }
---
+++ src/components/Operacion.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+
+export default function Operacion(props) {
+  const [num1, setNum1] = useState(generateRandomNumber());
+  const [num2, setNum2] = useState(generateRandomNumber());
+  const [userInput, setUserInput] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState(null);
+  const [message, setMessage] = useState('');
+  const [operacion, setOperacion] = useState('');
+
+  useEffect(() => {
+    resetearNumeros();
+  }, [props.op]);
+
+  useEffect(() => {
+    if (props.op === '+') {
+      setOperacion('Suma');
+      setCorrectAnswer(num1 + num2);
+    } else if (props.op === '-') {
+      setOperacion('Resta');
+      setCorrectAnswer(num1 - num2);
+    } else if (props.op === '/') {
+      setOperacion('Divide');
+      const newNum2 = generateRandomNumber(2, 10); 
+      const newNum1 = newNum2 * generateRandomNumber(1, 10); //De esta manera nos aseguramos que siempre sea una division sin resto
+      setNum1(newNum1);
+      setNum2(newNum2);
+      setCorrectAnswer(newNum1 / newNum2);
+    } else if (props.op === '*') {
+      setOperacion('Multiplica');
+      const newNum2 = generateRandomNumber(1,10); 
+      const newNum1 = newNum2 * generateRandomNumber(1, 10); //De esta manera nos aseguramos que siempre sea una division sin resto
+      setNum1(newNum1);
+      setNum2(newNum2);
+      setCorrectAnswer(newNum1 * newNum2);
+    }
+  }, [props.op, num1, num2]);
+
+  const resetearNumeros = () => {
+    setNum1(generateRandomNumber());
+    setNum2(generateRandomNumber());
+    setUserInput('');
+  };
+
+  const handleCheckAnswer = () => {
+    if (parseInt(userInput) === correctAnswer) {
+      setMessage('😀¡Respuesta correcta!😀');
+      resetearNumeros();
+
+      //Esperamos 2.5s y limpiamos el mensaje
+      setTimeout(() => {
+        setMessage('');
+      }, 2500);
+    } else {
+      setMessage('💀Respuesta incorrecta💀. Inténtalo de nuevo.');
+    }
+  };
+
+  function generateRandomNumber(min = 1, max = 100) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  return (
+    <div>
+      <h2>{operacion} los siguientes números:</h2>
+      <p>{num1} {props.op} {num2}</p>
+      <input type="number" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+      <button className="btn" onClick={handleCheckAnswer}>Comprobar</button>
+      <p>{message}</p>
+    </div>
+  );
+}
